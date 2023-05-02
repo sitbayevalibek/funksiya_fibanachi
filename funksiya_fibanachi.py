de8ddff1291ee3581e87ac489de3a1a6dcbfef2b
sitbayevalibek <dizdada1881@gmail.com>
2023-05-02 21:32:20 +0500

    Function
    Function Fibanachi

---
+++ funksiya_fibanachi.py
@@ -0,0 +1,10 @@
+
+def fibanachi(n):
+    sonlar = []
+    for x in range(n):
+        if x==0 or x==1:
+            sonlar.append(1)
+        else:
+            sonlar.append(sonlar[x-1]+sonlar[x-2])
+    return sonlar
+print(fibanachi(10))
